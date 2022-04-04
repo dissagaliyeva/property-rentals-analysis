ef0ae8f6f26b129f8ff5620deb7c13d4ac71019c
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-04-04 21:12:07 +0400

    Add Adaboost (61%), XGBoost models (85%), and RandomForestRegressor (88%). TODO: modularize and add simple models

--- utils/models/create.py
+++ utils/models/create.py
@@ -1,8 +1,13 @@
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import GridSearchCV, train_test_split
-
 from utils.visualize import viz
+
+
+RANDOM_STATE = 42
 
 
 def random_forest(df, features=False):
@@ -17,8 +22,12 @@ def random_forest(df, features=False):
 
     X, y = df.drop('price', axis='columns'), df['price']
     X = pd.get_dummies(X, drop_first=True)
+
+    # scale y
+    y = np.log1p(y)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
-                                                        random_state=101)
+                                                        random_state=RANDOM_STATE)
     best = hyper_params(rf, X_train, y_train).best_params_
     print('Best params:', best)
 
@@ -26,7 +35,8 @@ def random_forest(df, features=False):
                                max_depth=best['max_depth'],
                                min_samples_leaf=best['min_samples_leaf'],
                                max_features=best['max_features'],
-                               bootstrap=True)
+                               bootstrap=True,
+                               random_state=RANDOM_STATE)
     sort = None
 
     if features:
@@ -63,10 +73,22 @@ def hyper_params(model, x, y, name='rf'):
         grid_rf = GridSearchCV(estimator=model, param_grid=params_rf, cv=3,
                                scoring='neg_mean_squared_error',
                                verbose=1, n_jobs=-1, refit=True).fit(x, y)
+        print('Best params:', grid_rf.best_params_)
         return grid_rf
 
-    if name == 'xgb':
+    if name == 'xgboost':
         pass
+
+    if name == 'adaboost':
+        dt = DecisionTreeRegressor(max_depth=1, random_state=RANDOM_STATE)
+        hyperparameter_space = {'n_estimators': list(range(2, 102, 2)),
+                                'learning_rate': np.arange(0.1, 1, 0.1)}
+
+        gs = GridSearchCV(AdaBoostRegressor(base_estimator=dt,
+                                            random_state=RANDOM_STATE),
+                          param_grid=hyperparameter_space,
+                          scoring='neg_mean_squared_error', n_jobs=-1, cv=5)
+        print('Best params:', gs.best_params_)
 
 
 def choose_features(model, x, y):
