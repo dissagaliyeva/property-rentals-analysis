c3d4a53dd38fcb0fe9eb857a92dea2ba64bb036c
Dinara Issagaliyeva <dinarissaa@gmail.com>
2022-04-05 19:04:30 +0400

    Finish putting together the notebook

--- utils/visualize/viz.py
+++ utils/visualize/viz.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from utils import stats
+import ppscore as pps
 
 
 def corr_matrix(df, col=None):
@@ -146,4 +147,17 @@ def show_importance(sort):
     plt.xlabel('Column', fontsize=14)
     plt.ylabel('Importance', fontsize=14)
     plt.tight_layout()
+    plt.show()
+
+
+def pps_matrix(df):
+    # Calculate pps
+    pps_matrix = pps.matrix(df)
+    # Prepare data to pivot table
+    pps_pivot = pps_matrix.pivot('x', 'y', 'ppscore')
+    pps_pivot.index.name, pps_pivot.columns.name = None, None
+    # Plot
+    plt.figure(figsize=(10, 4))
+    sns.heatmap(pps_pivot, annot=True, cmap='YlGn')
+    plt.title('Predictive Power Score Matrix', fontsize=16);
     plt.show()
